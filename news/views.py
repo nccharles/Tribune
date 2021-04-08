41bc8e0ac0c64463729597b386827df0770b255a
Charles NDAYISABA <nccharles1@gmail.com>
2021-04-09 00:56:19 +0200

    updating views

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
@@ -16,22 +17,23 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
-def news_of_day(request):
-    date = dt.date.today()
-    return render(request, 'all-news/today-news.html', {"date": date,})
 
-def past_days_news(request,past_date):
-    # Converts data from the string Url
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
+def past_days_news(request, past_date):
     try:
         # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
     except ValueError:
         # Raise 404 error when ValueError is thrown
         raise Http404()
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
-   
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
